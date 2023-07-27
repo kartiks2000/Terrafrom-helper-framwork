e0d32ab282372f6a65959180cb23cb8cf55c6b9b
Kartik saxena <kartiksaxena@kartiks-mbp.home>
2023-07-27 19:23:41 -0400

    build: deploying servers

--- main.tf
+++ main.tf
@@ -1,8 +1,38 @@
-# resource "random_string" "random_suffix" {
-#   length  = 8
-#   special = false
-#   upper   = false
-# }
+module "web-server-1" {
+    source = "./modules/ec2"
+
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.medium"
+    name = "web-server-1"
+    subnet_id = module.network-1.public_subnet_ids[0]
+}
+
+module "web-server-2" {
+    source = "./modules/ec2"
+
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.medium"
+    name = "web-server-2"
+    subnet_id = module.network-1.public_subnet_ids[1]
+}
+
+module "backend-server-1" {
+    source = "./modules/ec2"
+
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.medium"
+    name = "backend-server-2"
+    subnet_id = module.network-1.private_subnet_ids[0]
+}
+
+module "backend-server-2" {
+    source = "./modules/ec2"
+
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.medium"
+    name = "backend-server-2"
+    subnet_id = module.network-1.private_subnet_ids[1]
+}
 
 
 
--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIASZTATQXGGLOQE3MG"
-  secret_key = "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"
+  access_key = "AKIAXQ6LGCL75O5HH7S5"
+  secret_key = "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"
 }
